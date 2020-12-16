3a417592d37ef00c0abfcf0c5ac0bb351cdd3af3
Prashoon123 <prashoonbhattacharjee@gmail.com>
2020-12-16 21:57:00 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeamoq6GCAGQdAMFZ1gPpNKvCLIBSSknA",
-  authDomain: "twitter-clone-abc.firebaseapp.com",
-  projectId: "twitter-clone-abc",
-  storageBucket: "twitter-clone-abc.appspot.com",
-  messagingSenderId: "415845717957",
-  appId: "1:415845717957:web:45265b31809024c5055dac",
-  measurementId: "G-MLCNS850M0",
+  // ENTER YOUR APP'S FIREBASE CONFIG HERE
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
